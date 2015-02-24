ad0a1c91381a868c5005dfedb983c1e9a6ea6375
Sung Chi <sungchi91@gmail.com>
2015-02-24 14:38:47 -0800

    signup updated

--- ver1.0/js/signup.js
+++ ver1.0/js/signup.js
@@ -1,9 +1,23 @@
 (function() {
-	//employed btn on click, hide stuff thats not relevant to employee 
-
-	var jobtitle = document.getElementById("jT");
-	//console.log(jobtitle.style);
-
-	jobtitle.style.display = "hidden";
-
+	var employed = document.getElementById("employedSelection");
+	var jobseeker= document.getElementById("jobSeekerSelection");	
+	var student = document.getElementById("studentSelection");
+	
+	window.div_OnOff =function (value){
+		 if(value == "employed"){
+			employed.style.display = "";
+			jobseeker.style.display = "none";
+			student.style.display = "none";
+		}
+		 if(value == "looking"){
+			employed.style.display = "none";
+			jobseeker.style.display = "";
+			student.style.display = "none";
+		}
+		else if(value == "student"){
+			employed.style.display = "none";
+			jobseeker.style.display = "none";
+			student.style.display = "";
+			}
+	};
 })();
