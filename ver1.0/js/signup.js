78b9fda67f10a4ebeb1c09a2e84fc93179c84a21
DuyNguyen <blinkblinklink@aim.com>
2015-02-25 22:34:43 -0800

    fixed signup.js

--- ver1.0/js/signup.js
+++ ver1.0/js/signup.js
@@ -6,7 +6,11 @@
 
 	var signup_options = document.getElementsByClassName("signup-option");
 	
-	function showOption(value){
+	function showOption(){
+
+		var value = this.value;	//THIS LINE IS DIFFERENT 
+		
+
 		 if(value == "employed"){
 			employed.style.display = "";
 			jobseeker.style.display = "none";
@@ -23,13 +27,37 @@
 			student.style.display = "";
 			}
 	};
-	for(var i; i<signup_options.length; i++){
-	    signup_options[i].addEventListener("click",showOption(this.value));
+	for(var i = 0; i<signup_options.length; i++){
+	    signup_options[i].addEventListener("click",showOption);
 	}
-	/*
-	signup_options[0].addEventListener("click",showOption(signup_options[0].value));
-	signup_options[1].addEventListener("click",showOption(signup_options[1].value));
+	
+	//employed.addEventListener("change",showOption(signup_options[0].value));
+	/*signup_options[1].addEventListener("click",showOption(signup_options[1].value));
 	signup_options[2].addEventListener("click",showOption(signup_options[2].value));
 	*/
+
+
+	// HOW I WOULD WRITE THIS
+	// for(var i = 0; i<signup_options.length; i++){
+	//     signup_options[i].addEventListener("click",function(){
+	//     	var value = this.value;
+	// 	 if(value == "employed"){
+	// 		employed.style.display = "";
+	// 		jobseeker.style.display = "none";
+	// 		student.style.display = "none";
+	// 	}
+	// 	 if(value == "looking"){
+	// 		employed.style.display = "none";
+	// 		jobseeker.style.display = "";
+	// 		student.style.display = "none";
+	// 	}
+	// 	else if(value == "student"){
+	// 		employed.style.display = "none";
+	// 		jobseeker.style.display = "none";
+	// 		student.style.display = "";
+	// 		}
+	//     });
+	// }
+
 
 })();
