1ec5e3227180b097159d202f64bda44cbe178cd0
Hung Tran <hung.d.tran@sjsu.edu>
2015-02-27 02:35:29 -0800

    refractor loggin page and signup page

--- signup/EmailVerification.php
+++ signup/EmailVerification.php
@@ -1,7 +1,16 @@
 <?php
-include '../lib/php/classes/AccountAdmin.php';
+require_once __DIR__.'/../lib/php/sqlConnection.php';
+require_once __DIR__.'/../lib/php/classes/AccountAdmin.php';
+
+if ( $_GET['VerificationKey'] && $_GET])
 
 $VerificationKey = $_GET['VerificationKey'];
+$Email = $_GET['Email'];
 
 $admin = new AccountAdmin();
+if ($admin->verifyAccount($Email, $VerificationKey)) {
+	echo 'success';
+} else {
+	echo 'Could not verify account.';
+}
 ?>
