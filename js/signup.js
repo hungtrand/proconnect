5e0e04521a89f6b71396c703e87f1b7bc8e7f568
Sung Chi <sungchi91@gmail.com>
2015-02-28 01:45:53 -0800

    index input validation

--- js/signup.js
+++ js/signup.js
@@ -3,9 +3,8 @@ $(document).ready(function() {
 	var jobseeker= document.getElementById("jobSeekerSelection");	
 	var student = document.getElementById("studentSelection");
 
-
-	var signup_options = document.getElementsByClassName("signup-option");
-	
+	//show options by usertype
+	var signup_options = document.getElementsByClassName("signup-option");	
 	function showOption(){
 
 		var value = this.value;
@@ -31,28 +30,8 @@ $(document).ready(function() {
 	    signup_options[i].addEventListener("click",showOption);
 	}
 	
-
-	// HOW I WOULD WRITE THIS
-	// for(var i = 0; i<signup_options.length; i++){
-	//     signup_options[i].addEventListener("click",function(){
-	//     	var value = this.value;
-	// 	 if(value == "employed"){
-	// 		employed.style.display = "";
-	// 		jobseeker.style.display = "none";
-	// 		student.style.display = "none";
-	// 	}
-	// 	 if(value == "looking"){
-	// 		employed.style.display = "none";
-	// 		jobseeker.style.display = "";
-	// 		student.style.display = "none";
-	// 	}
-	// 	else if(value == "student"){
-	// 		employed.style.display = "none";
-	// 		jobseeker.style.display = "none";
-	// 		student.style.display = "";
-	// 		}
-	//     });
-	// }
-
+	//validate input
+	
+	
 
 })();
