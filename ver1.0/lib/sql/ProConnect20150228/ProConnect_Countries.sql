36bbf1145f6effd119e90405fb23d4407fa59d50
LEoN <skyversal@gmail.com>
2015-02-28 20:33:38 -0800

    added list of countries table

---
+++ ver1.0/lib/sql/ProConnect20150228/ProConnect_Countries.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `Countries` (
+  `CountryID` int(11) NOT NULL AUTO_INCREMENT,
+  `CountryStandard` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`CountryID`)
+) ENGINE=InnoDB AUTO_INCREMENT=256 DEFAULT CHARSET=latin1;
+SELECT * FROM ProConnect.Countries;
