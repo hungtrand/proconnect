dac4870f367d4fedb6095653a9e383993df99d4a
LEoN <skyversal@gmail.com>
2015-04-03 22:46:23 -0700

    Project_controller added

--- profile-user-POV/php/Project_controller.php
+++ profile-user-POV/php/Project_controller.php
@@ -1,13 +1,13 @@
 <?php
-error_reporting(E_ALL); // debug
-ini_set("display_errors", 1); // debug
+//error_reporting(E_ALL); // debug
+//ini_set("display_errors", 1); // debug
 
 require_once __DIR__."/../../lib/php/sqlConnection.php";
 require_once __DIR__."/../../lib/php/classes/Account.php";
 require_once __DIR__."/../../lib/php/classes/User.php";
 require_once __DIR__."/../../lib/php/classes/Project.php";
 
-/*
+
 
 // checking if logged in
 session_start();
@@ -22,11 +22,11 @@ if (!$User = new Project($id)) {
 	echo "The Id is not in the database";
 	die();
 }
-*/
+
 	$name = null;
 	$url = null;
 	$description = null;
-	$uid = 2;
+
 	//$uid = $UData[3];
 	
 
@@ -47,8 +47,8 @@ if (!$User = new Project($id)) {
 
 		$proj = new Project();
 		//var_dump($proj->getData());
-		if($proj->load($uid)){
-			$proj = new Project($uid);
+		if($proj->load($id)){
+			$proj = new Project($id);
 			$proj->setProjectTitle($name);
 			$proj->setProjectURL($url);
 			$proj->setDescription($description);
