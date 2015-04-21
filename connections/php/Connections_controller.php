a48e8cbdca3c8e60d87f7fcd8efc2baae3b8c94e
Hung Tran <hung.d.tran@sjsu.edu>
2015-04-20 17:24:54 -0700

    connections edited for mobile android

--- connections/php/Connections_controller.php
+++ connections/php/Connections_controller.php
@@ -1,21 +1,32 @@
 <?php
 //error_reporting(E_ALL); // debug
 //ini_set("display_errors", 1); // debug
+require_once __DIR__."/../../lib/php/classes/Account.php";
+require_once __DIR__."/../../lib/php/classes/AccountAdmin.php";
 require_once __DIR__."/../../lib/php/sqlConnection.php";
 require_once __DIR__."/../../lib/php/classes/User.php";
 require_once __DIR__."/../../lib/php/classes/UserConnectionManager.php";
 require_once __DIR__."/Connections_view.php";
 
-// Check if logged in
-session_start();
-if (!$UData = json_decode($_SESSION['__USERDATA__'], true)) {
-	echo 'Session Timed Out. <a href="/signin/">Sign back in</a>';
-	die();
+if (isset($_POST['Username']) && isset($_POST['Password'])) {
+	$login = $_POST['Username'];
+	$password = $_POST['Password'];
+	$accAdm = new AccountAdmin();
+
+	$acc = $accAdm->getAccount($login, $password);
+	$uid = $acc->getUserID();
+} else {
+	session_start();
+	$home = 'Location: ../../';
+	if (!$UData = json_decode($_SESSION['__USERDATA__'], true)) {
+		//header($home);
+		echo 'Session Timed Out. <a href="/signin/">Sign back in</a>';
+		die();
+	}
+
+	$uid = $UData['USERID'];
 }
 
-// Check if data valid or still exists in the database
-$uid = $UData['USERID'];
-//$uid = 10;
 if (!$User = new User($uid)) {
 	header($home);
 	die();
