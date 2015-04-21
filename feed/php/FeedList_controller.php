649837b19496eb795f822986469b2c78a60269df
Hung Tran <hung.d.tran@sjsu.edu>
2015-04-20 17:34:45 -0700

    Feedlist for android

--- feed/php/FeedList_controller.php
+++ feed/php/FeedList_controller.php
@@ -2,23 +2,35 @@
 // error_reporting(E_ALL); // debug
 // ini_set("display_errors", 1); // debug
 require_once __DIR__."/../../lib/php/sqlConnection.php";
+require_once __DIR__."/../../lib/php/classes/Account.php";
+require_once __DIR__."/../../lib/php/classes/AccountAdmin.php";
 require_once __DIR__."/../../lib/php/classes/User.php";
 require_once __DIR__."/../../lib/php/classes/Feed.php";
 require_once __DIR__."/../../lib/php/classes/Feed2UserManager.php";
 require_once __DIR__."/FeedList_view.php";
 
 // Check if logged in
-/*session_start();
-if (!$UData = json_decode($_SESSION['__USERDATA__'], true)) {
-	echo 'Session Timed Out. <a href="/signin/">Sign back in</a>';
-	die();
+if (isset($_POST['Username']) && isset($_POST['Password'])) {
+	$login = $_POST['Username'];
+	$password = $_POST['Password'];
+	$accAdm = new AccountAdmin();
+
+	$acc = $accAdm->getAccount($login, $password);
+	$uid = $acc->getUserID();
+} else {
+	session_start();
+	$home = 'Location: ../../';
+	if (!$UData = json_decode($_SESSION['__USERDATA__'], true)) {
+		//header($home);
+		echo 'Session Timed Out. <a href="/signin/">Sign back in</a>';
+		die();
+	}
+
+	$uid = $UData['USERID'];
 }
 
-// Check if data valid or still exists in the database
-$uid = $UData['USERID'];*/
-$uid = 7;
 if (!$User = new User($uid)) {
-	header($home);
+	echo 'Session Timed Out. <a href="/signin/">Sign back in</a>';
 	die();
 }
 
