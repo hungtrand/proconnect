09f083bc981d9430dd52beb9e64118b20ffae9b6
Hung Tran <hung.d.tran@sjsu.edu>
2015-04-26 15:59:14 -0700

    Public-Profile-POV backend done waiting to merge

--- profile-public-POV/php/Profile_controller.php
+++ profile-public-POV/php/Profile_controller.php
@@ -8,6 +8,7 @@ require_once __DIR__."/../../lib/php/classes/EducationManager.php";
 require_once __DIR__."/../../lib/php/classes/ExperienceManager.php";
 require_once __DIR__."/../../lib/php/classes/ProjectManager.php";
 require_once __DIR__."/../../lib/php/classes/SkillManager.php";
+require_once __DIR__."/../../lib/php/classes/UserConnectionManager.php";
 require_once __DIR__."/Profile_view.php";
 
 // Check if logged in
@@ -27,29 +28,50 @@ if (!$User = new User($uid)) {
 }
 
 //$User = new User(10); // For Testing
+if (!isset($_POST['UserID'])) {
+	echo "Invalid Request";
+	die();
+}
+
+$targetUID = $_POST['UserID'];
+
+if (!$targetUser = new User($targetUID)) {
+	echo 'User Not Found.';
+	die();
+}
+
 
 // get all info for profile
 $Acc = new Account();
-$Acc->loadByUserID($User->getID());
+$Acc->loadByUserID($targetUser->getID());
 
-$ExpMgr = new ExperienceManager($User);
+$ExpMgr = new ExperienceManager($targetUser);
 $AllExp = $ExpMgr->getAll();
 
-$EduMgr = new EducationManager($User);
+$EduMgr = new EducationManager($targetUser);
 $AllEdu = $EduMgr->getAll();
 
-$ProjMgr = new ProjectManager($User);
+$ProjMgr = new ProjectManager($targetUser);
 $AllProj = $ProjMgr->getAll();
 
-$SkillMgr = new SkillManager($User);
+$SkillMgr = new SkillManager($targetUser);
 $AllSkills = $SkillMgr->getAll();
+
+$conn = new Connection();
+$connStatus = "Not Connected";
+if ($conn->loadByUsers($uid, $CUserID)) {
+	if ($conn->getAccepted()) $connStatus = "Connected";
+	else $connStatus = "Invitation Pending.";
+}
+
 
 // Start Producing View here
 $view = new Profile_View();
-$view->loadPersonalInfo($User, $Acc);
+$view->loadPersonalInfo($targetUser, $Acc);
 $view->loadExperience($AllExp);
 $view->loadProjects($AllProj);
-$view->loadEducation($AllEdu);	
+$view->loadEducation($AllEdu);
+$view->setConnectionStatus($connStatus);	
 
 echo json_encode($view->getView());
 ?>
