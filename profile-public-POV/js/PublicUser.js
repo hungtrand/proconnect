8387b4b5c314ce7b36005f45e584360f23fa2f5a
Hung Tran <hung.d.tran@sjsu.edu>
2015-04-27 13:38:42 -0700

    pull before you push

--- profile-public-POV/js/PublicUser.js
+++ profile-public-POV/js/PublicUser.js
@@ -117,8 +117,8 @@ PublicUser.prototype = {
 
 		//do an ajax call to get user data
 		$.ajax({
-			//url: "php/Profile_controller.php",
-			 url: "php/dummy.php",
+			url: "php/Profile_controller.php",
+			 // url: "php/dummy.php",
 			method: 'POST',
 			data: that.queryString(),   			// <----------- data is {"userID": 11111} 
 			error: function(xhr,status,error) {
@@ -158,9 +158,7 @@ PublicUser.prototype = {
 		$(".first-name").text(this.userData.personalInfo["first-name"]);
 		$("#user-mi").text(this.userData.personalInfo["middle-initial"]+'.');
 		$("#user-last").text(this.userData.personalInfo["last-name"]);
-		$("#user-address").text(this.userData.personalInfo["user-address"]["address-input"]);
-			$("#user-address").append(" ");
-			$("#user-address").append(this.userData.personalInfo["user-address"]["country-input"]);
+		$("#user-address").text(this.userData.personalInfo["user-address"]);
 		$("#user-email").text(this.userData.personalInfo["email-address"]);
 		$("#user-phone").text(this.userData.personalInfo["phone-number"]);
 		$("#user-home").text(this.userData.personalInfo["phone-number"]);
