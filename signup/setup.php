595f5155da97212b9e9bc7e7d0c9354e1f3364d5
Sung Chi <sungchi91@gmail.com>
2015-05-04 10:55:57 -0700

    footer removed on mobile view

--- signup/setup.php
+++ signup/setup.php
@@ -254,7 +254,7 @@
 		</div> <!-- /container -->
 	</div>
 		<!-- Footer -->
-  <footer class="footer">
+  <footer class="footer hidden-xs">
         <strong>ProConnect</strong> v1.0 &copy; Copyright 2015
     </footer>
     <!-- // Footer -->
